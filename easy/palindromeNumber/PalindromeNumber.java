97e84c2466f9b5dd86bf37e11c2f5ebd2b274314
Yevhen Nesterenko <nesterenkoj@gmail.com>
2022-11-08 23:28:47 +0200

    PalindromeNumber solution done

--- easy/palindromeNumber/PalindromeNumber.java
+++ easy/palindromeNumber/PalindromeNumber.java
@@ -1,28 +1,34 @@
 package com.leetcode.easy.palindromeNumber;
 
-import java.util.Arrays;
-
 public class PalindromeNumber {
     public static boolean isPalindrome(int x) {
-        boolean flag = false;
-        while (x > 0) {
-            int[] array = new int[(int) (Math.log10(x) + 1)];
-            System.out.println(Arrays.toString(array));
-            for (int i = array.length - 1; i >= 0; i--) {
-                array[i] = x % 10;
-                x = x / 10;
-            }
-            int count = 0;
-            for (int i = 0; i < array.length; i++) {
-                if (array[i] == array[array.length - 1 - i]) {
-                    count++;
-                }
-            }
-            if (count >= array.length / 2 +1) flag = true;
 
-            System.out.println(Arrays.toString(array));
-            System.out.println(flag);
+        if (x == 0) return true;
+        if (x < 0) return false;
+        boolean flag = false;
+
+
+        int[] array = new int[(int) (Math.log10(x) + 1)];
+
+        if (array.length == 1) return true;
+
+        for (int i = array.length - 1; i >= 0; i--) {
+            array[i] = x % 10;
+            x = x / 10;
         }
+
+        int boom = array.length;
+        int count = 0;
+
+        for (int i = 0; i < array.length / 2; i++) {
+            if (array[i] == array[boom - 1]) {
+                boom -= 1;
+                count ++;
+            }
+        }
+
+        if (count == array.length/2) flag = true;
+
         return flag;
     }
 }
@@ -30,6 +36,6 @@ public class PalindromeNumber {
 class PalidromeTest {
     public static void main(String[] args) {
         PalindromeNumber.isPalindrome(
-                1000021);
+                1001);
     }
 }
